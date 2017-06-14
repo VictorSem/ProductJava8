3ff18beef9c8e1bfb93edc199296a82974570214
Victor <viktorsemashko87@gmail.com>
2017-06-14 09:53:12 +0300

    update

--- ProductJava8/src/main/java/org/example/pojoCollections/ProductCollections.java
+++ ProductJava8/src/main/java/org/example/pojoCollections/ProductCollections.java
@@ -17,7 +17,7 @@ public class ProductCollections {
 	private ArrayList<Product> prodCollection = new ArrayList<>();
 
 	private Stream<Product> products() {
-		return getAllProduct().stream();
+		return getProdCollection().stream();
 	}
 
 	public int size() {
@@ -28,32 +28,26 @@ public class ProductCollections {
 		return prodCollection.get(i);
 	}
 
-	public boolean isEmpty() {
+	public boolean isEmpty() { 
 		return prodCollection.isEmpty();
 	}
 
-	public void setProd(ArrayList<Product> prodCollection) {
-		this.prodCollection = prodCollection;
-	}
-
-	public ArrayList<Product> getAllProduct() {
-		if (getProd().isEmpty()) {
-			getProd().add(new Product().setName("Икра красная \"Вкуснота \"").setBrand("Белорусская традиция").setPrice(25));
-			getProd().add(new Product().setName("Чай зеленый \"Принцесса Нури\"").setBrand("Ахмад").setPrice(15));
-			getProd().add(new Product().setName("Чай ароматный с бергамотом.").setBrand("Беседа").setPrice(1000));
-			getProd().add(new Product().setName("Чай ароматный с бергамотом.").setBrand("Беседа").setPrice(65));
-			getProd().add(new Product().setName("Lipton Yellow Label").setBrand("Lipton").setPrice(50));
-			getProd().add(new Product().setName("Magic aroma").setBrand("Lipton").setPrice(18));
-			getProd().add(new Product().setName("English Breakfast").setBrand("Lipton").setPrice(12));
-			getProd().add(new Product().setName("Royal Ceylon").setBrand("Lipton").setPrice(14));
+	public ArrayList<Product> getProdCollection() {
+		if (isEmpty()) {
+			prodCollection.add(new Product().setName("Икра красная \"Вкуснота \"").setBrand("Наши традиции").setPrice(25));
+			prodCollection.add(new Product().setName("Чай зеленый \"Принцесса Нури\"").setBrand("Ахмад").setPrice(15));
+			prodCollection.add(new Product().setName("Чай ароматный с бергамотом.").setBrand("Беседа").setPrice(140));
+			prodCollection.add(new Product().setName("Чай ароматный с бергамотом.").setBrand("Беседа").setPrice(65));
+			prodCollection.add(new Product().setName("Lipton Yellow Label").setBrand("Lipton").setPrice(50));
+			prodCollection.add(new Product().setName("Magic aroma").setBrand("Lipton").setPrice(18));
+			prodCollection.add(new Product().setName("English Breakfast").setBrand("Lipton").setPrice(12));
+			prodCollection.add(new Product().setName("Royal Ceylon").setBrand("Lipton").setPrice(14));
 		}
 
 		return prodCollection;
 	}
 	
-	public ArrayList<Product> getProd() {
-		return prodCollection;
-	}
+	
 
 	@Override
 	public String toString() {
@@ -82,9 +76,8 @@ public class ProductCollections {
 				.collect(Collectors.toList());
 
 		if (list.isEmpty()) {
-			log.info("Dont find element. ");
+			log.info("Dont find element.");
 		}
-
 		return list;
 	}
 	
