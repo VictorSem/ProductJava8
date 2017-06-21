4dd420a422a8b9f50e8231b676b75d16215d3172
Victor <viktorsemashko87@gmail.com>
2017-06-21 16:32:18 +0300

    Update MainClasss.java
    add examples with anyMatch/noneMatch/allMatch

--- ProductJava8/src/main/java/org/example/main/MainClasss.java
+++ ProductJava8/src/main/java/org/example/main/MainClasss.java
@@ -1,26 +1,38 @@
 package org.example.main;
 
+import static org.example.pojoCollections.ProductCollection.productBrand;
+import static org.example.pojoCollections.ProductCollection.productName;
+import static org.example.pojoCollections.ProductCollection.productPrice;
+
 import org.apache.log4j.Logger;
-import org.example.pojoCollections.ProductCollections;
-import static org.example.pojoCollections.ProductCollections.productBrand;
-import static org.example.pojoCollections.ProductCollections.productName;
-import static org.example.pojoCollections.ProductCollections.productPrice;
+import org.example.pojoCollections.ProductCollection;
 
 public class MainClasss {
-	
+
 	private static final Logger log = Logger.getLogger(MainClasss.class);
-		
+
 	public static void main(String[] args) {
-		ProductCollections pc = new ProductCollections();	
-		
-		log.info(pc.prodWithMaxPrice()); 
+		ProductCollection pc = new ProductCollection();
+
+		log.info(pc.prodWithMaxPrice());
 		log.info(pc.prodWithMinPrice());
-		
+
 		log.info("=========================");
-		
+
 		log.info(pc.findBy(productBrand("Беседа")));
 		log.info(pc.findBy(productName("Ceylon")));
 		log.info(pc.findBy(productPrice(50)));
+		
+		
+		log.info("=========================");
+		
+		log.info(pc.isAnyProductPriceGreaterOrEqual(100)); //true
+		log.info(pc.isNoneProductPriceGreaterOrEqual(200)); //true
+		log.info(pc.isAllProductPriceGreaterOrEqual(100)); //false
+		
+		
+
+		
 	}
 
 }
